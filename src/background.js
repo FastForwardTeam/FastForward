a2efe05d03fd424ab85c1ff8f614e190db121c2b
abhishek gupta <ag251994@gmail.com>
2018-03-26 17:11:31 +0530

    First run page link updated
    First run page link was broken. So updated it from '/pages/firstrun.html' to '/html/firstrun.html'

--- src/background.js
+++ src/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function(details)
 {
 	if(details.reason == "install")
 	{
-		window.open(chrome.extension.getURL("/pages/firstrun.html"));
+		window.open(chrome.extension.getURL("/html/firstrun.html"));
 	}
 });
 chrome.runtime.setUninstallURL("https://goo.gl/forms/H8FswYQ2a37LSxc13");
