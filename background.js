dd41b547c108fd46380d00c413a39294f5f911b3
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-07-28 04:39:50 +0200

    Bypass situsbaru.me (fixes #308)

--- background.js
+++ background.js
@@ -281,7 +281,8 @@ brws.webRequest.onBeforeRequest.addListener(details=>{
 },{types:["main_frame"],urls:[
 "*://*.yumechan.club/?r=*",
 "*://*.celeclub.org/?r=*",
-"*://*.duniaislamku.com/?r=*"
+"*://*.duniaislamku.com/?r=*",
+"*://*.situsbaru.me/?r=*"
 ]},["blocking"])
 
 brws.webRequest.onBeforeRequest.addListener(details=>{
