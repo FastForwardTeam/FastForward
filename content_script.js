59ef58812f78224dfbc3cb155a409003f9a3e2d9
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-08-19 17:41:29 +0200

    Add arubedo.blogspot.com bypass (fixes #357)

--- content_script.js
+++ content_script.js
@@ -779,7 +779,7 @@ if(document instanceof HTMLDocument)
 						}, 200)
 					}, 200)
 				})
-				domainBypass(/get-click2\\.blogspot\\.com|kisechan\\.web\\.id/,()=>{
+				domainBypass(/get-click2\\.blogspot\\.com|kisechan\\.web\\.id|arubedo\\.blogspot\\.com/,()=>{
 					let u=aesCrypto.decrypt(convertstr(location.href.substr(location.href.indexOf("?o=")+3)),convertstr("root"))
 					if(isGoodLink(u))
 					{
