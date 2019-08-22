a6280e1ea92666adcda7807a231ea8df4a264d57
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-08-22 21:33:02 +0200

    Add hokiciki.org bypass (fixes #352)

--- content_script.js
+++ content_script.js
@@ -419,6 +419,9 @@ if(document instanceof HTMLDocument)
 					set:_=>safelyNavigate(decodeURIComponent(atob(_)))
 				})
 			})
+			domainBypass("hokiciki.org",()=>{
+				ifElement("a[href^='/get-link/']",a=>location.href=a.href)
+			})
 			//Insertion point 1 — insert bypasses running before the DOM is loaded above this comment
 			hrefBypass(/njiir\\.com|linkduit\\.net|k2s\\.cc|1link\\.club|cshort\\.(org|xyz)|muhammadyoga\\.me|u\\.to|skiplink\\.io|healthykk\\.com|punchsubs\\.net|linkasm\\.com|firefaucet\\.win\\/l\\/|emulator\\.games\\/download\\.php|2speed\\.net\\/file\\//,()=>{
 				window.setInterval=f=>setInterval(f,1)
