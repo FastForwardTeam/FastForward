439c3bffb1790d4411ca67e824950950a3318498
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-11-26 16:17:37 +0100

    Add filepuma.com bypass (closes #664)

--- content_script.js
+++ content_script.js
@@ -520,7 +520,7 @@ if(document instanceof HTMLDocument)
 			})
 			//Insertion point 1 — insert bypasses running before the DOM is loaded above this comment
 			hrefBypass(/(njiir|healthykk|linkasm|dxdrive)\\.com|punchsubs\\.net|k2s\\.cc|muhammadyoga\\.me|u\\.to|skiplink\\.io|firefaucet\\.win\\/l\\/|emulator\\.games\\/download\\.php/,()=>window.setInterval=f=>setInterval(f,1))
-			domainBypass(/(racaty|longfiles)\\.com|indishare\\.org|datei\\.to/,()=>window.setTimeout=f=>setTimeout(f,1))
+			domainBypass(/(racaty|longfiles|filepuma)\\.com|indishare\\.org|datei\\.to/,()=>window.setTimeout=f=>setTimeout(f,1))
 			if(bypassed)
 			{
 				return
