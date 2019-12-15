273997b397b148e029afd30cabaa98380de1811a
thankyou73409 <58903065+thankyou73409@users.noreply.github.com>
2019-12-15 16:10:09 +0700

    Add acortalo.live bypass

--- content_script.js
+++ content_script.js
@@ -532,6 +532,17 @@ if(document instanceof HTMLDocument)
 					},1000)
 				})
 			})
+			domainBypass(/acortalo\\.(live|xyz)/,()=>{
+				if(document.referrer.indexOf("megawarez")>-1)
+				{
+					const _ce=document.createElement
+					document.createElement=t=>{
+						let e=_ce.call(document,t)
+						e.submit=()=>safelyNavigate(data)
+						return e
+					}
+				}
+			})
 			//Insertion point 1 — insert bypasses running before the DOM is loaded above this comment
 			hrefBypass(/(njiir|healthykk|linkasm|dxdrive|getwallpapers)\\.com|punchsubs\\.net|k2s\\.cc|muhammadyoga\\.me|u\\.to|skiplink\\.io|firefaucet\\.win\\/l\\/|emulator\\.games\\/download\\.php/,()=>window.setInterval=f=>setInterval(f,1))
 			domainBypass(/(racaty|longfiles|filepuma|filehorse|portableapps)\\.com|indishare\\.org|datei\\.to/,()=>window.setTimeout=f=>setTimeout(f,1))
