08b001deea56ce68905420eb3849a1a34e10178c
Tim "timmyRS" Speckhals <github@timmyrs.de>
2020-01-08 04:32:02 +0100

    Add keisekai.fun bypass (closes #814)

--- injection_script.js
+++ injection_script.js
@@ -544,7 +544,7 @@ domainBypass("apkpsp.com",()=>{
 })
 //Insertion point 1 — insert bypasses running before the DOM is loaded above this comment
 hrefBypass(/(njiir|healthykk|linkasm|dxdrive|getwallpapers)\.com|punchsubs\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|firefaucet\.win\/l\/|emulator\.games\/download\.php/,()=>window.setInterval=f=>setInterval(f,1))
-domainBypass(/(racaty|longfiles|filepuma|filehorse|portableapps)\.com|indishare\.org|datei\.to/,()=>window.setTimeout=f=>setTimeout(f,1))
+domainBypass(/(racaty|longfiles|filepuma|filehorse|portableapps)\.com|indishare\.org|datei\.to|keisekai\.fun/,()=>window.setTimeout=f=>setTimeout(f,1))
 if(bypassed)
 {
 	return
