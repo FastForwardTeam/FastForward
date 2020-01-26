3d4ed2fa14c4e658cc9016cdfba44109d57b1d04
Tim "timmyRS" Speckhals <github@timmyrs.de>
2020-01-26 14:37:04 +0100

    Add tinyurl.majidzhacker.com bypass (closes #872)

--- injection_script.js
+++ injection_script.js
@@ -807,7 +807,7 @@ ensureDomLoaded(()=>{
 			}, 200)
 		}, 200)
 	})
-	domainBypass(/((get-click2|informations-library|media-blue|akashirohige)\.blogspot|business\.ominfoupdate)\.com|pastikan\.me|blog\.infolanjutan\.xyz/,()=>{
+	domainBypass(/((get-click2|informations-library|media-blue|akashirohige)\.blogspot|business\.ominfoupdate|tinyurl\.majidzhacker)\.com|pastikan\.me|blog\.infolanjutan\.xyz/,()=>{
 		let u=aesCrypto.decrypt(convertstr(location.href.substr(location.href.indexOf("?o=")+3)),convertstr("root"))
 		if(isGoodLink(u))
 		{
