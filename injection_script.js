20c7c994004d94a8ff21208b3f2afcf0c1e5f4c7
Tim "timmyRS" Speckhals <github@timmyrs.de>
2020-02-07 09:00:34 +0100

    Improve shirosafe.web.id bypass (re #900)

--- injection_script.js
+++ injection_script.js
@@ -1101,7 +1101,7 @@ ensureDomLoaded(()=>{
 			safelyNavigate(decodeURIComponent(url.search.split("url=")[1].split("&")[0]))
 		}
 	}))
-	domainBypass("shirosafe.web.id",()=>ifElement("center > a[href^='https://shirosafe.web.id/']",a=>safelyAssign(a.href)))
+	domainBypass("shirosafe.web.id",()=>ifElement("a#klik[href^='#']",a=>ifElement("div"+a.getAttribute("href")+"[style='display:none;margin-top: 30px;'] > div[id] > center > a[href^='https://shirosafe.web.id/']",a=>safelyAssign(a.href))))
 	domainBypass(/bebasdownloadfilm\.com|dl\.sharemydrive\.xyz/,()=>ifElement("frame[src*='/iframe/top.php?']",f=>{
 		f.onload=()=>safelyNavigate(f.contentDocument.querySelector("p#skip a").href)
 	}))
