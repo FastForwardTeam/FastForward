0d6a76882c186261bf0df35e25e811d657bd8391
Tim "timmyRS" Speckhals <github@timmyrs.de>
2020-02-10 15:05:26 +0100

    Improve shirosafe.web.id bypass (re #900)

--- injection_script.js
+++ injection_script.js
@@ -575,7 +575,7 @@ domainBypass("ipeenk.id",()=>{
 domainBypass("lkc21.net",()=>window.setTimeout=f=>setTimeout(f,100))
 //Insertion point 1 — insert bypasses running before the DOM is loaded above this comment
 hrefBypass(/(njiir|healthykk|linkasm|dxdrive|getwallpapers|sammobile)\.com|punchsubs\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|uploadfree\.info|firefaucet\.win\/l\/|emulator\.games\/download\.php/,()=>window.setInterval=f=>setInterval(f,1))
-hrefBypass(/(racaty|longfiles|filepuma|filehorse|portableapps)\.com|indishare\.org|datei\.to|keisekai\.fun|solvetube\.site|shirosafe\.web\.id|nexusmods\.com\/.*\/mods\/[0-9]*\?tab=files&file_id=[0-9]*/,()=>window.setTimeout=f=>setTimeout(f,1))
+hrefBypass(/(racaty|longfiles|filepuma|filehorse|portableapps)\.com|indishare\.org|datei\.to|keisekai\.fun|solvetube\.site|nexusmods\.com\/.*\/mods\/[0-9]*\?tab=files&file_id=[0-9]*/,()=>window.setTimeout=f=>setTimeout(f,1))
 domainBypass("universal-bypass.org",()=>{
 	window.universalBypassInstalled=true
 	window.universalBypassInternalVersion=UNIVERSAL_BYPASS_INTERNAL_VERSION
@@ -1210,6 +1210,7 @@ ensureDomLoaded(()=>{
 			safelyAssign(fileDownloadLoca)
 		}
 	})
+	domainBypass("shirosafe.web.id",()=>ifElement("a#klik[href^='#']",a=>ifElement("div"+a.getAttribute("href")+"[style='display:none;margin-top: 30px;'] > div[id] > center > a[href^='https://shirosafe.web.id/'][style='margin-bottom: 15px; width: 180px;']:not([href='https://shirosafe.web.id/18057d888094e505ZIAiaca0fb6244fb7a7ad'])",a=>safelyAssign(a.href))))
 	//Insertion point 2 — insert bypasses running after the DOM is loaded above this comment
 	if(bypassed)
 	{
