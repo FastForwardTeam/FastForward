473277b936f68532a0d3de96744eb9cdb3ee2c4b
ikancupang <58947597+ikancupang@users.noreply.github.com>
2020-02-12 23:01:27 +0700

    Add ydfile.com bypass (closes #927)

--- injection_script.js
+++ injection_script.js
@@ -577,7 +577,7 @@ domainBypass("ipeenk.id",()=>{
 domainBypass("lkc21.net",()=>window.setTimeout=f=>setTimeout(f,100))
 hrefBypass(/emulator\.games\/download\.php/,()=>window.setInterval=f=>setInterval(f,100))
 //Insertion point 1 — insert bypasses running before the DOM is loaded above this comment
-domainBypass(/^((www\.)?((njiir|healthykk|linkasm|dxdrive|getwallpapers|sammobile)\.com|punchsubs\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|uploadfree\.info))$/,()=>window.setInterval=f=>setInterval(f,1))
+domainBypass(/^((www\.)?((njiir|healthykk|linkasm|dxdrive|getwallpapers|sammobile|ydfile)\.com|punchsubs\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|uploadfree\.info))$/,()=>window.setInterval=f=>setInterval(f,1))
 hrefBypass(/firefaucet\.win\/l\/|sfirmware\.com\/downloads-file\//,()=>window.setInterval=f=>setInterval(f,1))
 domainBypass(/^((www\.)?((racaty|longfiles|filepuma|filehorse|portableapps)\.com|indishare\.org|datei\.to|keisekai\.fun|solvetube\.site))$/,()=>window.setTimeout=f=>setTimeout(f,1))
 hrefBypass(/nexusmods\.com\/.*\/mods\/[0-9]*\?tab=files&file_id=[0-9]*$/,()=>window.setTimeout=f=>setTimeout(f,1))
