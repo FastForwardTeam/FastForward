9220e9bdb26c937d3a0f5b12e9c22305a0156e7e
Tim "timmyRS" Speckhals <github@timmyrs.de>
2020-02-12 21:02:49 +0100

    Fix emulator.games bypass https://github.com/timmyRS/Universal-Bypass/commit/65ae594ba51938ec679d189b0dd6c8fb6b8a2ede#r37257333

--- injection_script.js
+++ injection_script.js
@@ -580,7 +580,7 @@ hrefBypass(/firefaucet\.win\/l\/|sfirmware\.com\/downloads-file\//,()=>window.se
 domainBypass(/^((www\.)?((racaty|longfiles|filepuma|filehorse|portableapps)\.com|indishare\.org|datei\.to|keisekai\.fun|solvetube\.site))$/,()=>window.setTimeout=f=>setTimeout(f,1))
 hrefBypass(/nexusmods\.com\/.*\/mods\/[0-9]*\?tab=files&file_id=[0-9]*$/,()=>window.setTimeout=f=>setTimeout(f,1))
 domainBypass("lkc21.net",()=>window.setTimeout=f=>setTimeout(f,100))
-hrefBypass(/(emulator\.games\/download\.php)$/,()=>window.setInterval=f=>setInterval(f,100))
+hrefBypass(/emulator\.games\/download\.php/,()=>window.setInterval=f=>setInterval(f,100))
 domainBypass("universal-bypass.org",()=>{
 	window.universalBypassInstalled=true
 	window.universalBypassInternalVersion=UNIVERSAL_BYPASS_INTERNAL_VERSION
