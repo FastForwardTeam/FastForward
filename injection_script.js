7b23818585d89793e34553d371dcd6e991344789
ikancupang <58947597+ikancupang@users.noreply.github.com>
2020-02-13 13:25:21 +0700

    Improve shrink.world bypass (re #863)

--- injection_script.js
+++ injection_script.js
@@ -898,7 +898,7 @@ ensureDomLoaded(()=>{
 		f.submit()
 	})
 	domainBypass("xxx.lewd.ninja",()=>safelyNavigate(document.body.textContent))
-	domainBypass(/tr\.link|movienear\.me|lewat\.club|droidtamvan\.me|(liveshootv|modebaca)\.com|shrink\.world|link\.mymastah\.xyz/,()=>{
+	domainBypass(/tr\.link|movienear\.me|lewat\.club|droidtamvan\.me|(liveshootv|modebaca)\.com|shrink\.world|link\.mymastah\.xyz|sportif\.id/,()=>{
 		if(typeof app_vars=="undefined")
 		{
 			app_vars={}
@@ -1701,7 +1701,7 @@ ensureDomLoaded(()=>{
 			}
 			domainBypass("adcoinfly.com",()=>document.querySelectorAll("a.btn[href=''], a.btn[href*='clickme']").forEach(e=>e.parentNode.removeChild(e)))
 			domainBypass(/(semawur|bercara)\.com|in11\.site/,()=>ifElement("input[type='hidden'][name='alias'][value]",i=>crowdPath(i.value),()=>crowdPath(location.hash.substr(1))))
-			domainBypass(/movienear\.me|lewat\.club|droidtamvan\.me|(liveshootv|modebaca)\.com|shrink\.world|link\.mymastah\.xyz/,()=>{
+			domainBypass(/movienear\.me|lewat\.club|droidtamvan\.me|(liveshootv|modebaca)\.com|shrink\.world|link\.mymastah\.xyz|sportif\.id/,()=>{
 				ifElement("input[type='hidden'][name='alias'][value]",i=>{
 					i.parentNode.action+="#"+i.value+(ignoreCrowdBypass?"#ignoreCrowdBypass":"")
 					crowdPath(i.value)
