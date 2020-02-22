cbeb8044f3e4e57e18d775dc4178f882849e263c
ikancupang <58947597+ikancupang@users.noreply.github.com>
2020-02-22 11:27:07 +0700

    Defuse soralink autoLink JS (re #807)

--- injection_script.js
+++ injection_script.js
@@ -587,6 +587,10 @@ domainBypass("zt-protect.com",()=>{
 		ifElement("a[href] > .showURL",p=>safelyNavigate(p.parentNode.href))
 	}
 })
+//Soralink autoLink JS
+ODP(window,"soralink",{
+	get:()=>{}
+})
 //Insertion point 1 — insert bypasses running before the DOM is loaded above this comment
 domainBypass(/^((www\.)?((njiir|healthykk|linkasm|dxdrive|getwallpapers|sammobile|ydfile)\.com|punchsubs\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|uploadfree\.info))$/,()=>window.setInterval=f=>setInterval(f,1))
 hrefBypass(/firefaucet\.win\/l\/|sfirmware\.com\/downloads-file\//,()=>window.setInterval=f=>setInterval(f,1))
