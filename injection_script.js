db9c0decc3fd55162d063f0725b1d35a4e4b90ab
Tim "timmyRS" Speckhals <github@timmyrs.de>
2020-04-01 19:01:12 +0200

    Add apkily.com bypass (closes #1094)

--- injection_script.js
+++ injection_script.js
@@ -621,7 +621,7 @@ domainBypass("4shared.com",()=>{
 })
 //Insertion point for bypasses running before the DOM is loaded.
 domainBypass(/^((www\.)?((njiir|healthykk|linkasm|dxdrive|getwallpapers|sammobile|ydfile)\.com|punchsubs\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|uploadfree\.info|fstore\.biz))$/,()=>window.setInterval=f=>setInterval(f,1))
-hrefBypass(/firefaucet\.win\/l\/|sfirmware\.com\/downloads-file\//,()=>window.setInterval=f=>setInterval(f,1))
+hrefBypass(/firefaucet\.win\/l\/|sfirmware\.com\/downloads-file\/|(apkily\.com\/getapp$)/,()=>window.setInterval=f=>setInterval(f,1))
 hrefBypass(/emulator\.games\/download\.php|curseforge\.com\/.*\/download\/[0-9]*/,()=>window.setInterval=f=>setInterval(f,100))
 domainBypass(/^((www\.)?((racaty|longfiles|filepuma|portableapps)\.com|indishare\.org|datei\.to|keisekai\.fun|solvetube\.site))$/,()=>window.setTimeout=f=>setTimeout(f,1))
 domainBypass("lkc21.net",()=>window.setTimeout=f=>setTimeout(f,100))
