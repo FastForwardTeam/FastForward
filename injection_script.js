8bf110b9484a5dbbf0a3928da6ffed3e9b3d518c
ikancupang <58947597+ikancupang@users.noreply.github.com>
2020-04-28 00:44:09 +0700

    Add 5play.ru bypass (closes #1183)

--- injection_script.js
+++ injection_script.js
@@ -1363,6 +1363,7 @@ ensureDomLoaded(()=>{
 		})
 	})
 	domainBypass("uploadking.net",()=>ifElement("form[name='F1']",f=>countIt(()=>f.submit())))
+	domainBypass("5play.ru",()=>ifElement("div.download-result > a[href]",a=>safelyNavigate(a.href)))
 	//Insertion point for domain-or-href-specific bypasses running after the DOM is loaded. Bypasses here will no longer need to call ensureDomLoaded.
 	if(bypassed)
 	{
