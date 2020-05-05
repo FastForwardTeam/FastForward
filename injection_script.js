bcb5e41f6cee9bf630b4c0f713a5346cc76aad6b
Sainan <github@sainan.de>
2020-05-05 12:23:05 +0200

    Add boost.ink bypass

--- injection_script.js
+++ injection_script.js
@@ -1364,6 +1364,7 @@ ensureDomLoaded(()=>{
 	})
 	domainBypass("uploadking.net",()=>ifElement("form[name='F1']",f=>countIt(()=>f.submit())))
 	domainBypass("5play.ru",()=>ifElement("div.download-result > a[href]",a=>safelyNavigate(a.href)))
+	domainBypass("boost.ink",()=>ifElement("script[version]",s=>safelyNavigate(atob(s.getAttribute("version")))))
 	//Insertion point for domain-or-href-specific bypasses running after the DOM is loaded. Bypasses here will no longer need to call ensureDomLoaded.
 	if(bypassed)
 	{
