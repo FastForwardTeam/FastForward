7e232bfe687bc0c84a328a004f3ea999553e69a6
Sainan <github@sainan.de>
2020-05-13 17:04:37 +0200

    Update Firefox build process

--- .firefox_build.php
+++ .firefox_build.php
@@ -1,40 +1,7 @@
 <?php
-function recursivelyDelete($path)
-{
-	if(substr($path, -1) == "/")
-	{
-		$path = substr($path, 0, -1);
-	}
-	if(!file_exists($path))
-	{
-		return;
-	}
-	if(is_dir($path))
-	{
-		foreach(scandir($path) as $file)
-		{
-			if(!in_array($file, [
-				".",
-				".."
-			]))
-			{
-				recursivelyDelete($path."/".$file);
-			}
-		}
-		rmdir($path);
-	}
-	else
-	{
-		unlink($path);
-	}
-}
 if(file_exists("Universal Bypass for Netscape Navigator.zip"))
 {
 	unlink("Universal Bypass for Netscape Navigator.zip");
-}
-if(is_dir(".firefox"))
-{
-	recursivelyDelete(".firefox");
 }
 
 echo "Indexing...\n";
@@ -49,7 +16,6 @@ function recursivelyIndex($dir)
 			$fn = $dir."/".$f;
 			if(is_dir($fn))
 			{
-				mkdir(".firefox/".$fn);
 				recursivelyIndex($fn);
 			}
 			else
@@ -59,7 +25,6 @@ function recursivelyIndex($dir)
 		}
 	}
 }
-mkdir(".firefox");
 recursivelyIndex(".");
 
 echo "Building...\n";
@@ -83,11 +48,9 @@ foreach($index as $fn)
 	if($fn == "manifest.json")
 	{
 		$json = json_decode(file_get_contents($fn), true);
-		unset($json["browser_specific_settings"]);
 		$json["version"] .= ".".$build_id;
 		$content = str_replace("    ", "\t", json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 		$build->addFromString($fn, $content);
-		file_put_contents(".firefox/".$fn, $content);
 	}
 	else if($fn == "background.js")
 	{
@@ -102,18 +65,11 @@ foreach($index as $fn)
 			"if(false)"
 		], file_get_contents($fn));
 		$build->addFromString($fn, $content);
-		file_put_contents(".firefox/".$fn, $content);
 	}
 	else
 	{
 		$build->addFile($fn, $fn);
-		copy($fn, ".firefox/".$fn);
 	}
 }
 $build->close();
 file_put_contents(".next_build_id.txt", $build_id + 1);
-if(is_file(".sign_firefox_build.bat"))
-{
-	passthru(".sign_firefox_build.bat");
-}
-recursivelyDelete(".firefox");
