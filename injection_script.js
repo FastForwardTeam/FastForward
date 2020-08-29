39037195f16d18ac644e58ae26eb47d34c097b50
GitHub Web Flow <19864447+web-flow@users.noreply.github.com>
2020-08-30 03:13:52 +0700

    Improve shorte.st bypass

--- injection_script.js
+++ injection_script.js
@@ -1403,7 +1403,7 @@ ensureDomLoaded(()=>{
 	}))
 	domainBypass("favpng.com",()=>ifElement("div#countdown",()=>safelyNavigate("https://files.favpng.com/api_download.php?k="+location.pathname.substr(14))))
 	domainBypass("sh.st",()=>ifElement("[data-translate='block_headline']",()=>location.hostname="ceesty.com"))
-	domainBypass(/(ceesty|corneey|destyy|festyy|gestyy)\.com|viid\.me/,()=>{
+	domainBypass(/(ceesty|corneey|destyy|festyy|gestyy|clkmein|cllkme|jnw0|xiw34)\.com|(viid|clkme|wiid)\.me/,()=>{
 		if(typeof postCaptchaResponse=="function")
 		{
 			postCaptchaResponse(location.href,"")
