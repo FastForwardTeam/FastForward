47023e2b104dad206d99c838b17d768d498763c6
Sainan <63328889+Sainan@users.noreply.github.com>
2020-09-07 12:20:22 +0200

    Improve linegee.net bypass

--- injection_script.js
+++ injection_script.js
@@ -982,9 +982,7 @@ ensureDomLoaded(()=>{
 		})
 	})
 	domainBypass(/linkpoi\.(in|cc)/,()=>ifElement("a.btn.btn-primary[href]",a=>safelyNavigate(a.href)))
-	domainBypass(/spacetica\.com|linegee\.net/,()=>ifElement("a.btn.btn-xs[href]",a=>setTimeout(()=>{
-		$._data(a,"events").click[0].handler({preventDefault:()=>{},originalEvent:{}})
-	},1000)))
+	domainBypass(/spacetica\.com|linegee\.net/,()=>ifElement("a.btn.btn-primary[href]",a=>safelyNavigate(a.href)))
 	domainBypass(/uiz\.(io|app)|moon7\.xyz/,()=>crowdBypass(()=>{
 		awaitElement("#go-adsredirect",f=>{
 			f.action+="#bypassClipboard="+location.pathname.substr(1)
