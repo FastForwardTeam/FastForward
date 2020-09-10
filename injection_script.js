03271b7718ec33169cb21627ed2f52540f66eb76
Sainan <63328889+Sainan@users.noreply.github.com>
2020-09-10 22:22:48 +0200

    Fix bypasses in the wrong place

--- injection_script.js
+++ injection_script.js
@@ -1486,6 +1486,29 @@ ensureDomLoaded(()=>{
 			fun2()
 		}
 	})
+	domainBypass("oracle.com",()=>document.querySelectorAll("[data-file]").forEach(e=>{
+		//https://gist.github.com/wavezhang/ba8425f24a968ec9b2a8619d7c2d86a6#gistcomment-3377085
+		let link=e.getAttribute("data-file"),
+		jre8=RegExp("download.oracle.com/otn/java/jdk/8u([0-9]*)-b([0-9]*)/([a-z0-9]{32})/(.*)$","g").exec(link)
+		if(jre8&&jre8[3])
+		{
+			os_type=RegExp("8u[0-9]*-([^-]*)-").exec(jre8[4])[1]
+			os_type=(os_type == "macosx")?"unix":os_type
+			e.onclick=()=>safelyNavigate("https://javadl.oracle.com/webapps/download/GetFile/1.8.0_"+jre8[1]+"-b"+jre8[2]+"/"+jre8[3]+"/"+os_type+"-i586/"+jre8[4])
+		}
+	}))
+	domainBypass("genlink.cc",()=>{
+		$(".check-ad").append("<input name='step' value=2 type='hidden'>")
+		let b=$(".real-link")
+		if(b.attr("href"))
+		{
+			safelyNavigate(b.attr("href"))
+		}
+		else
+		{
+			b.click()
+		}
+	})
 	//Insertion point for domain-or-href-specific bypasses running after the DOM is loaded. Bypasses here will no longer need to call ensureDomLoaded.
 	if(bypassed)
 	{
@@ -2009,29 +2032,6 @@ ensureDomLoaded(()=>{
 		safelyNavigate(i.value)
 		finish()
 	},()=>domainBypass(/seputarinfomenarik\.com|(massardi|kribboy)\.xyz/,()=>ifElement("a#hapus",a=>safelyAssign(a.href))))
-	domainBypass("oracle.com",()=>document.querySelectorAll("[data-file]").forEach(e=>{
-		//https://gist.github.com/wavezhang/ba8425f24a968ec9b2a8619d7c2d86a6#gistcomment-3377085
-		let link=e.getAttribute("data-file"),
-		jre8=RegExp("download.oracle.com/otn/java/jdk/8u([0-9]*)-b([0-9]*)/([a-z0-9]{32})/(.*)$","g").exec(link)
-		if(jre8&&jre8[3])
-		{
-			os_type=RegExp("8u[0-9]*-([^-]*)-").exec(jre8[4])[1]
-			os_type=(os_type == "macosx")?"unix":os_type
-			e.onclick=()=>safelyNavigate("https://javadl.oracle.com/webapps/download/GetFile/1.8.0_"+jre8[1]+"-b"+jre8[2]+"/"+jre8[3]+"/"+os_type+"-i586/"+jre8[4])
-		}
-	}))
-	domainBypass("genlink.cc",()=>{
-		$(".check-ad").append("<input name='step' value=2 type='hidden'>")
-		let b=$(".real-link")
-		if(b.attr("href"))
-		{
-			safelyNavigate(b.attr("href"))
-		}
-		else
-		{
-			b.click()
-		}
-	})
 	//Insertion point for bypasses detecting certain DOM elements. Bypasses here will no longer need to call ensureDomLoaded.
 	let t=document.querySelector("title")
 	if(!bypassed&&t)
