db38264c8a321347c878704401d7b757673f20a7
respecting <62519659+respecting@users.noreply.github.com>
2020-10-20 20:37:45 -0600

    Add linksunlocked.com bypass

--- injection_script.js
+++ injection_script.js
@@ -724,6 +724,13 @@ domainBypass(/pahe\.(in|me|ph)/,()=>{
 		a.setAttribute("data-bypass-clipboard",s)
 	}))
 })
+domainBypass("linksunlocked.com",()=>{
+	const searchParams=new URLSearchParams(location.search)
+	if(searchParams.has("token"))
+	{
+		safelyNavigate("https://uploadhaven.com/download/"+searchParams.get("token"))
+	}
+})
 //Insertion point for bypasses running before the DOM is loaded.
 domainBypass(/^((www\.)?((njiir|healthykk|linkasm|dxdrive|getwallpapers|sammobile|ydfile)\.com|(punchsubs|zedge|fex)\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|(uploadfree|freeupload)\.info|fstore\.biz))$/,()=>window.setInterval=f=>setInterval(f,1))
 hrefBypass(/thesimsresource\.com\/downloads\/details\/id\//,()=>window.setTimeout=f=>setTimeout(f,1))
