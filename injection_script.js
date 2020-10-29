59dd624aa4a1c6ac05a30851d3820aade88db1ba
Sainan <63328889+Sainan@users.noreply.github.com>
2020-10-29 13:38:44 +0100

    Add file hoster info to asdfiles.com (closes #1371)

--- injection_script.js
+++ injection_script.js
@@ -749,7 +749,7 @@ ensureDomLoaded(()=>{
 	{
 		persistHash("ignoreCrowdBypass")
 	}
-	domainBypass(/^((www\.)?(file(factory|-upload)\.com|up-load\.io|cosmobox\.org|rockfile\.co))$/,()=>insertInfoBox("{{msg.infoFileHoster}}"))
+	domainBypass(/^((www\.)?((file(factory|-upload)|asdfiles)\.com|up-load\.io|cosmobox\.org|rockfile\.co))$/,()=>insertInfoBox("{{msg.infoFileHoster}}"))
 	domainBypass(/linkvertise\.(com|net)|link-to\.net/,()=>insertInfoBox(UNIVERSAL_BYPASS_INTERNAL_VERSION>=9?"{{msg.infoLinkvertise}}":"We're not allowed to bypass this website but we have negotiated the removal of their most annoying steps."))
 	domainBypass(/adfoc\.us|ads\.bdcraft\.net/,()=>ifElement(".skip[href]",b=>safelyNavigate(b.href)))
 	domainBypass("srt.am",()=>{
