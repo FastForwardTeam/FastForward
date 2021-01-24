c55c1765b778e81fc73fbec0cbea472b469df975
Sainan <63328889+Sainan@users.noreply.github.com>
2021-01-24 09:07:31 +0100

    Remove superfluous calls to ensureDomLoaded

--- injection_script.js
+++ injection_script.js
@@ -1610,9 +1610,9 @@ ensureDomLoaded(()=>{
 		}
 	})
 	domainBypass("pnd.money",()=>keepLooking(()=>ifElement("form#pnd_redirect_form, form#link-view",f=>f.submit())))
-	domainBypass("5mod-file.ru",()=>ensureDomLoaded(()=>ifElement("a.download",a=>safelyNavigate(a.href))))
-	domainBypass("kazanclilink.com",()=>ensureDomLoaded(()=>ifElement("a#baglantigit",a=>safelyNavigate(a.href))))
-	domainBypass("moddingunited.xyz",()=>ensureDomLoaded(()=>{if(typeof reverseString=="function")safelyNavigate(reverseString(window.location.search.substring(1)))}))
+	domainBypass("5mod-file.ru",()=>ifElement("a.download",a=>safelyNavigate(a.href)))
+	domainBypass("kazanclilink.com",()=>ifElement("a#baglantigit",a=>safelyNavigate(a.href)))
+	domainBypass("moddingunited.xyz",()=>{if(typeof reverseString=="function")safelyNavigate(reverseString(window.location.search.substring(1)))})
 	//Insertion point for domain-or-href-specific bypasses running after the DOM is loaded. Bypasses here will no longer need to call ensureDomLoaded.
 	if(bypassed)
 	{
