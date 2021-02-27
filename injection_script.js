8f63749525c81709dce02e9e064619c9d341b8c2
RobbyAJM <robbyalvianjayamulia@gmail.com>
2021-02-28 00:15:03 +0700

    Add kooi.xyz bypass (closes #1798)

--- injection_script.js
+++ injection_script.js
@@ -735,6 +735,7 @@ domainBypass("linksunlocked.com",()=>{
 })
 domainBypass("samehadaku.vip",()=>ifElement("div.close-button",a=>a.click()))
 domainBypass("playhindi.com",()=>transparentProperty("downloadButton",safelyNavigate))
+domainBypass("kooi.xyz",()=>ifElement("a#link_download",safelyNavigate))
 //Insertion point for bypasses running before the DOM is loaded.
 domainBypass(/^((www\.)?((njiir|healthykk|linkasm|dxdrive|getwallpapers|sammobile|ydfile|mobilemodsapk|dlandroid|download\.modsofapk)\.com|(punchsubs|zedge|fex)\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|(uploadfree|freeupload)\.info|fstore\.biz))$/,()=>window.setInterval=f=>setInterval(f,1))
 hrefBypass(/thesimsresource\.com\/downloads\/details\/id\//,()=>window.setTimeout=f=>setTimeout(f,1))
