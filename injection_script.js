bbbdc6bbf74250b96e17f00ccfe293e795a6fc4f
Sainan <63328889+Sainan@users.noreply.github.com>
2021-02-27 18:20:27 +0100

    Fix kooi.xyz bypass placement

--- injection_script.js
+++ injection_script.js
@@ -735,7 +735,6 @@ domainBypass("linksunlocked.com",()=>{
 })
 domainBypass("samehadaku.vip",()=>ifElement("div.close-button",a=>a.click()))
 domainBypass("playhindi.com",()=>transparentProperty("downloadButton",safelyNavigate))
-domainBypass("kooi.xyz",()=>ifElement("a#link_download",safelyNavigate))
 //Insertion point for bypasses running before the DOM is loaded.
 domainBypass(/^((www\.)?((njiir|healthykk|linkasm|dxdrive|getwallpapers|sammobile|ydfile|mobilemodsapk|dlandroid|download\.modsofapk)\.com|(punchsubs|zedge|fex)\.net|k2s\.cc|muhammadyoga\.me|u\.to|skiplink\.io|(uploadfree|freeupload)\.info|fstore\.biz))$/,()=>window.setInterval=f=>setInterval(f,1))
 hrefBypass(/thesimsresource\.com\/downloads\/details\/id\//,()=>window.setTimeout=f=>setTimeout(f,1))
@@ -1635,6 +1634,7 @@ ensureDomLoaded(()=>{
 		}
 	})
 	domainBypass("droidfilehost.com",()=>{if(typeof wt !== 'undefined')wt=1})
+	domainBypass("kooi.xyz",()=>ifElement("a#link_download",safelyNavigate))
 	//Insertion point for domain-or-href-specific bypasses running after the DOM is loaded. Bypasses here will no longer need to call ensureDomLoaded.
 	if(bypassed)
 	{
