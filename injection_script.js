a8d4cc7b4efe6035bb3bdc8f1109bdf75003fa37
ElJaviLuki <eljaviluki@gmail.com>
2021-08-25 18:05:48 +0200

    Add bypass for pcgamestorrents.org

--- injection_script.js
+++ injection_script.js
@@ -749,7 +749,7 @@ hrefBypass(/daominhha\.com\/download/,()=>{
 	str=str.replaceAll(",","=");
 	safelyAssign(atob(str));
 });
-hrefBypass(/bluemediafiles\.com\/url-generator\.php\?url=/,()=>{
+hrefBypass(/(bluemediafiles\.com|pcgamestorrents.org)\/url-generator\.php\?url=/,()=>{
 	window.setInterval=f=>setInterval(f,1)
 	transparentProperty("Time_Start",t=>t-5000)
 	awaitElement("input#nut[src]",i=>i.parentNode.submit())
