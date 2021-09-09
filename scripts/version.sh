c2b2ecfd0ecd86c04ce1a02125d56437f9ed21fa
NotAProton <notaproton8@gmail.com>
2021-09-09 18:47:36 +0530

    Make CI work with new versioning
    Make CI streamlined for nightly.link and new version system.

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,17 +7,20 @@ set -e
 manifest_loc=$1
 
 
-if [[ $# -lt 2 ]]; then
+if [[ $# -lt 2 ]]; then #Creates a normal versioned json using only version.txt
+
     version=$(cat ../../src/version.txt)
     sed -i '/"version":/c\  "version": "'$version'",' $1
     echo vesrioned $(basename $1) to $version
 
-  elif [ "$2" = "no" ]; then
+  elif [ "$2" = "nover" ]; then #Creates a non-versioned json using only number of commits
+
     version="0.$(git shortlog | grep -E '^[ ]+\w+' | wc -l)"
     sed -i '/"version":/c\  "version": "'$version'",' $1
     echo vesrioned $(basename $1) to $version
 
-  else
+  else #Creates a json using the number of commits and appends the second argument to it
+
     version="0.$(git shortlog | grep -E '^[ ]+\w+' | wc -l).$2"
     sed -i '/"version":/c\  "version": "'$version'",' $1
     echo vesrioned $(basename $1) to $version
