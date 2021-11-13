2988b7d2cd64507ad647b111f281d475b0af7469
NotAProton <notaproton8@gmail.com>
2021-11-13 17:24:14 +0530

    Revert "Add bypass for iir.ai"

--- src/js/injection_script.js
+++ src/js/injection_script.js
@@ -464,12 +464,6 @@ domainBypass("tei.ai", () => {
 	    safelyNavigate(link);
 	});
 });
-domainBypass("iir.ai", () => {
-  ensureDomLoaded(() => {
-    var link = document.querySelector("#link-view > button[type='submit']").onclick.toString().match(/(https?:\/\/.*)('|")/i)[1];
-    safelyNavigate(link);
-  });
-});
 domainBypass("shortly.xyz",()=>{
 	if(location.pathname.substr(0,3)=="/r/")
 	{
