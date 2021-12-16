27f0983bc6ec9e592cf926dee545709b44e02be5
NotAProton <notaproton8@gmail.com>
2021-12-16 10:21:55 +0530

    Improve URI encoding of storage injection

--- src/js/content_script.js
+++ src/js/content_script.js
@@ -129,7 +129,9 @@ if(document instanceof Document)
 		});
 		brws.storage.local.get('ff_clipboard', function(result) {
 			ffClipboard_stored = result.ff_clipboard
-			ffClipboard_stored = encodeURIComponent(ffClipboard_stored)
+
+			//encodeURIcomponent and replace whatever's not encoded, https://stackoverflow.com/a/16435373/17117909
+			ffClipboard_stored = encodeURIComponent(ffClipboard_stored).replace(/\-/g, "%2D").replace(/\_/g, "%5F").replace(/\./g, "%2E").replace(/\!/g, "%21").replace(/\~/g, "%7E").replace(/\*/g, "%2A").replace(/\'/g, "%27").replace(/\(/g, "%28").replace(/\)/g, "%29")
 			let script=document.createElement("script")
 			script.innerHTML=`(()=>{
 				const crowdEnabled=`+(res.crowdEnabled?"true":"false")+`,
