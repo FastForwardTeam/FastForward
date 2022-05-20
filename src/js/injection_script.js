1a91e8e3bb756a0c4e5466a8c7f0c9aa2c08e29a
reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2022-05-20 10:35:26 +0200

    Improve tei.ai bypass (#502)

--- src/js/injection_script.js
+++ src/js/injection_script.js
@@ -547,12 +547,6 @@ domainBypass(/bc\.vc|bcvc\.live/,()=>{
 	window.setInterval=f=>setInterval(f,1)
 	awaitElement("a#getLink:not([style^='display'])",a=>a.click())
 })
-domainBypass("tei.ai", () => {
-	ensureDomLoaded(() => {
-	    var link = atob(`aH${document.querySelector("#link-view [name='token']").value.split("aH").slice(1).join("aH")}`);
-	    safelyNavigate(link);
-	});
-});
 domainBypass("shortly.xyz",()=>{
 	if(location.pathname.substr(0,3)=="/r/")
 	{
@@ -2621,6 +2615,15 @@ domainBypass('apkadmin.com', () => {
     c.innerHTML = ''
   })
 })
+
+
+	domainBypass("tei.ai", () => {
+		const token = document.querySelector('#link-view [name="token"]').value;
+		const decoded = atob(token);
+		const page = decoded.split('http').pop();
+		const link = `http${page}`;
+		safelyNavigate(link);
+	});
 
 domainBypass("bowfile.com", () => {
 	const regex=/.*let next = "(http[^"]+)";.*/
