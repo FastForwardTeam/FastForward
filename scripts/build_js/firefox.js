c42592cc578809e9c51b0e3ee5ef64130a5e48d1
reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2022-05-20 14:15:06 +0200

    create the final step for firefox build process

---
+++ scripts/build_js/firefox.js
@@ -0,0 +1,20 @@
+const zipper = require('adm-zip');
+
+module.exports = async function ({versioning, destination, commit_number, version} = {}) {
+    console.log(`[FastForward.build.firefox] building the FireFox package`);
+    const z = new zipper();
+
+    z.addLocalFolder(`${process.cwd()}/${destination}`);
+
+    let output_file = `${process.cwd()}/build/dist/`;
+
+    if (!versioning)
+        output_file += `FastForward_firefox_${commit_number}_dev.xpi`;
+    else if ('nover' === versioning)
+        output_file += `FastForward_firefox_0.${commit_number}.xpi`;
+    else
+        output_file += `FastForward_${version}_firefox.xpi`;
+
+    await z.writeZipPromise(output_file, {});
+    console.log('[FastForward.build.firefox] Succesfull build the FireFox package: %s', output_file)
+}
