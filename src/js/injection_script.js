3026d7a64fffd824ca85db3e3a1ee17cd9fd11c3
reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2022-07-27 14:49:47 +0200

    better

--- src/js/injection_script.js
+++ src/js/injection_script.js
@@ -891,7 +891,7 @@ domainBypass("fastforward.team",()=>{
 	window.fastForwardExternalVersion = "FAST_FORWARD_EXTERNAL_VERSION"
 	window.fastForwardInjectionVersion = "FAST_FORWARD_INJECTION_VERSION"
 
-	if (location.pathname.includes('bypassed') || location.pathname.includes('navigate'))
+	if (['/bypassed', '/navigate'].includes(location.pathname))
 		location.assign(`https://universal-bypass.org${location.pathname}${location.search}`)
 })
 
