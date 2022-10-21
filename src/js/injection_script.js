7b2b2c4d81e47835fc08838b4dee0e80386f39dd
Ahmed Badawe <109165623+N0-0NE-Dev@users.noreply.github.com>
2022-10-22 10:36:35 +1100

    Added bypass for akwam.to (#720)
    * Added bypass for akwam.to
    
    * Changed the code to use safelyNavigate

--- src/js/injection_script.js
+++ src/js/injection_script.js
@@ -1976,6 +1976,10 @@ ensureDomLoaded(()=>{
 	})
 
 	//Insertion point for domain-or-href-specific bypasses running after the DOM is loaded. Bypasses here will no longer need to call ensureDomLoaded.
+	domainBypass("noon.khsm.io", () => {
+		const url = document.getElementsByClassName("download-link")[0].getAttribute("href")
+		safelyNavigate(url)
+	})
 	domainBypass("maxurlz.com", () => {
 		const regex = /(?<="href=')(.*)(?='>Click here)/
 		for (const script of document.getElementsByTagName("script")) {
