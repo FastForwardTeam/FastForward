7928f3deb594e4aa1cdebe6dee241ab710d0d136
reashetyrr <6582364+reashetyrr@users.noreply.github.com>
2022-11-19 14:52:00 +0100

    create the actual bypasses for complete2unlock & mydramalist

---
+++ src/bypasses/mydramalist.js
@@ -0,0 +1,17 @@
+import BypassDefinition from './BypassDefinition.js';
+
+export default class Mydramalist extends BypassDefinition {
+    constructor() {
+        super();
+
+    }
+
+    execute() {
+        const search_params = location.search;
+        const decoded_search_params = decodeURIComponent(search_params);
+        const full_url = decoded_search_params.replace('?q=', '');
+        this.helpers.safelyNavigate(full_url);
+    }
+}
+
+export const matches = ['mydramalist.com'];
