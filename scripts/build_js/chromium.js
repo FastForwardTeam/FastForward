ff69e694b6cff25e448ed35b0cbacbf5bf660fb6
Sankalp Prashanth <45770865+0xc60f@users.noreply.github.com>
2022-12-16 18:34:21 -0600

    Formatted all files to meet code style specified in CODE_STYLE.md

--- scripts/build_js/chromium.js
+++ scripts/build_js/chromium.js
@@ -1,25 +1,34 @@
-import esMain from "es-main";
+import esMain from 'es-main'
 if (esMain(import.meta))
-    throw new Error('This file is a build module, run the build.js file to actually build FastForward');
+  throw new Error(
+    'This file is a build module, run the build.js file to actually build FastForward'
+  )
 
-import zipper from "adm-zip";
-import * as path from "path";
+import zipper from 'adm-zip'
+import * as path from 'path'
 
-export default async function ({versioning, destination, commit_number, version} = {}) {
-    console.log(`[FastForward.build.chromium] building the Chromium package`);
-    const z = new zipper();
+export default async function ({
+  versioning,
+  destination,
+  commit_number,
+  version
+} = {}) {
+  console.log(`[FastForward.build.chromium] building the Chromium package`)
+  const z = new zipper()
 
-    z.addLocalFolder(destination);
+  z.addLocalFolder(destination)
 
-    let output_file = path.join(destination, '..', 'dist');
+  let output_file = path.join(destination, '..', 'dist')
 
-    if (!versioning)
-        output_file += `/FastForward_chromium_${commit_number}_dev.crx`;
-    else if ('nover' === versioning)
-        output_file += `/FastForward_chromium_0.${commit_number}.crx`;
-    else
-        output_file += `/FastForward_${version}_chromium.crx`;
+  if (!versioning)
+    output_file += `/FastForward_chromium_${commit_number}_dev.crx`
+  else if ('nover' === versioning)
+    output_file += `/FastForward_chromium_0.${commit_number}.crx`
+  else output_file += `/FastForward_${version}_chromium.crx`
 
-    await z.writeZipPromise(output_file, {});
-    console.log('[FastForward.build.chromium] Succesfull build the Chromium package: %s', output_file)
+  await z.writeZipPromise(output_file, {})
+  console.log(
+    '[FastForward.build.chromium] Succesfull build the Chromium package: %s',
+    output_file
+  )
 }
--- scripts/build_js/firefox.js
+++ scripts/build_js/firefox.js
@@ -1,26 +1,35 @@
-import esMain from "es-main";
+import esMain from 'es-main'
 if (esMain(import.meta))
-    throw new Error('This file is a build module, run the build.js file to actually build FastForward');
+  throw new Error(
+    'This file is a build module, run the build.js file to actually build FastForward'
+  )
 
-import zipper from 'adm-zip';
-import * as path from "path";
+import zipper from 'adm-zip'
+import * as path from 'path'
 // const zipper = require('adm-zip');
 
-export default async function ({versioning, destination, commit_number, version} = {}) {
-    console.log(`[FastForward.build.firefox] building the FireFox package`);
-    const z = new zipper();
+export default async function ({
+  versioning,
+  destination,
+  commit_number,
+  version
+} = {}) {
+  console.log(`[FastForward.build.firefox] building the FireFox package`)
+  const z = new zipper()
 
-    z.addLocalFolder(destination);
+  z.addLocalFolder(destination)
 
-    let output_file = path.join(destination, '..', 'dist');
+  let output_file = path.join(destination, '..', 'dist')
 
-    if (!versioning)
-        output_file += `/FastForward_firefox_${commit_number}_dev.xpi`;
-    else if ('nover' === versioning)
-        output_file += `/FastForward_firefox_0.${commit_number}.xpi`;
-    else
-        output_file += `/FastForward_${version}_firefox.xpi`;
+  if (!versioning)
+    output_file += `/FastForward_firefox_${commit_number}_dev.xpi`
+  else if ('nover' === versioning)
+    output_file += `/FastForward_firefox_0.${commit_number}.xpi`
+  else output_file += `/FastForward_${version}_firefox.xpi`
 
-    await z.writeZipPromise(output_file, {});
-    console.log('[FastForward.build.firefox] Succesfull build the FireFox package: %s', output_file)
+  await z.writeZipPromise(output_file, {})
+  console.log(
+    '[FastForward.build.firefox] Succesfull build the FireFox package: %s',
+    output_file
+  )
 }
