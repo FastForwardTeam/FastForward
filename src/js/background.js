7b43ab36161ab2a4c5b44deeb9ba350e7688fc84
NotAProton <notaproton8@gmail.com>
2023-03-27 17:59:30 +0530

    Fix redirection
    & -> &&  :-D

--- src/js/background.js
+++ src/js/background.js
@@ -1,27 +1,26 @@
-const brws = typeof browser !== "undefined" ? browser : chrome;
+const brws = typeof browser !== 'undefined' ? browser : chrome;
 
 function handleClick() {
   brws.runtime.openOptionsPage();
 }
 
 function ffclipboardClear() {
-  brws.storage.local.get({ ff_clipboard: "{}" }, function (data) {
+  brws.storage.local.get({ ff_clipboard: '{}' }, function (data) {
     brws.storage.local.set({ ff_clipboard: data.ff_clipboard });
   });
 }
 
 function firstrun() {
-  brws.tabs.create({ url: "https://fastforward.team/firstrun" });
+  brws.tabs.create({ url: 'https://fastforward.team/firstrun' });
   ffclipboardClear();
 }
 
 function preflight(details) {
   let url = new URL(details.url);
-
   //navigate
-  if ((url.hostname == "fastforward.team") & (url.pathname == "bypassed")) {
+  if (url.hostname == 'fastforward.team' && url.pathname == '/bypassed') {
     brws.tabs.update(details.tabId, {
-      url: decodeURIComponent(URLSearchParams(details.url).get("target")),
+      url: decodeURIComponent(url.searchParams.get('target')),
     });
   }
 }
@@ -29,6 +28,9 @@ function preflight(details) {
 brws.action.onClicked.addListener(handleClick);
 brws.runtime.onInstalled.addListener(firstrun);
 brws.runtime.onStartup.addListener(ffclipboardClear);
-brws.webNavigation.onCompleted.addListener((details) => preflight(details), {
-  url: [{ hostContains: "fastforward.team" }],
-});
+brws.webNavigation.onBeforeNavigate.addListener(
+  (details) => preflight(details),
+  {
+    url: [{ hostContains: 'fastforward.team' }],
+  }
+);
