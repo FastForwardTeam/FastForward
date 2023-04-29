395f672e3f8c4cdeda6d6b36fabe660d64479799
NotAProton <notaproton8@gmail.com>
2023-04-29 16:48:11 +0530

    Replace uikit with css on before-navigate
    Also fix formatting

--- src/html/base.js
+++ src/html/base.js
@@ -3,6 +3,3 @@ document.documentElement.setAttribute(
   'dir',
   brws.i18n.getMessage('@@bidi_dir')
 );
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.documentElement.className = 'uk-light uk-background-secondary';
-}
