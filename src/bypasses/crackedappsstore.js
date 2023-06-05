9fce843f3c8a13adfe8f40fb325cf9359d358c08
0xc60f <0xc60f@duck.com>
2023-06-04 21:08:25 -0500

    Added crackappsstore bypass
    MV2 bypass is broken but this works

---
+++ src/bypasses/crackedappsstore.js
@@ -0,0 +1,14 @@
+import BypassDefinition from './BypassDefinition.js'
+
+export default class Crackedappsstore extends BypassDefinition {
+    constructor() {
+        super()
+        // custom bypass required bases can be set here
+    }
+
+    execute() {
+        // todo: make execute function
+    }
+}
+
+export const matches = ['example.com']
