aca3b1652405b44a6d45ac12af9f081cf50bb186
Ame <76390419+driedpampas@users.noreply.github.com>
2023-09-17 01:13:48 +0300

    update files

--- src/html/consent.js
+++ src/html/consent.js
@@ -4,36 +4,39 @@ if (isFirefox) {
     browser.storage.local.get('consentStatus').then(function (data) {
         const consentStatus = data.consentStatus;
 
-        document.getElementById("consentButton").addEventListener("click", function () {
-            browser.storage.local.set({ 'consentStatus': 'granted' }).then(function () {
-                window.location.href = 'options.html';
-            });
-        });
-
-        document.getElementById("declineButton").addEventListener("click", function () {
-            browser.storage.local.set({ 'consentStatus': 'declined' }).then(function () {
-
-                const popup = document.getElementById("popup");
-                popup.style.display = "block";
-
-                let countdownValue = 5;
-                const countdownElement = document.getElementById("countdown");
-                const countdownInterval = setInterval(function () {
-                    countdownValue -= 1;
-                    countdownElement.textContent = countdownValue;
-
-                    if (countdownValue === 0) {
-                        browser.management.uninstallSelf();
-                    }
-                }, 1000);
-
-                document.getElementById("cancelButton").addEventListener("click", function () {
-                    popup.style.display = "none";
-
-                    clearInterval(countdownInterval);
+        if (consentStatus !== 'granted') {
+            document.getElementById("consentButton").addEventListener("click", function () {
+                browser.storage.local.set({ 'consentStatus': 'granted' }).then(function () {
+                    window.location.href = 'options.html';
                 });
             });
-        });
+
+            document.getElementById("declineButton").addEventListener("click", function () {
+                browser.storage.local.set({ 'consentStatus': 'declined' }).then(function () {
+                    const popup = document.getElementById("popup");
+                    popup.style.display = "block";
+
+                    let countdownValue = 5;
+                    const countdownElement = document.getElementById("countdown");
+                    const countdownInterval = setInterval(function () {
+                        countdownValue -= 1;
+                        countdownElement.textContent = countdownValue;
+
+                        if (countdownValue === 0) {
+                            browser.management.uninstallSelf();
+                        }
+                    }, 1000);
+
+                    document.getElementById("cancelButton").addEventListener("click", function () {
+                        popup.style.display = "none";
+                        clearInterval(countdownInterval);
+                    });
+                });
+            });
+        }
     });
-} else {window.location.href = 'options.html';}
+} else {
+    // Not Firefox, load options.html directly
+    window.location.href = 'options.html';
+}
 
