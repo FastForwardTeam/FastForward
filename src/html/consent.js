2ad644ce08878516eb87cef34c899b4259a486ce
Ame <76390419+driedpampas@users.noreply.github.com>
2023-09-17 13:06:57 +0300

    update files

--- src/html/consent.js
+++ src/html/consent.js
@@ -19,35 +19,18 @@ document.addEventListener("DOMContentLoaded", function () {
         browser.runtime.onInstalled.addListener(async (details) => {
             if (details.reason === "install") {
                 const consentStatus = await getConsentStatus();
-
+    
                 if (consentStatus !== 'granted') {
                     const consentButton = document.getElementById("consentButton");
                     consentButton.addEventListener("click", async function () {
                         await saveConsentStatus('granted');
                         window.location.href = 'options.html';
                     });
-
+    
                     const declineButton = document.getElementById("declineButton");
                     declineButton.addEventListener("click", async function () {
                         await saveConsentStatus('declined');
-                        const popup = document.getElementById("popup");
-                        popup.style.display = "block";
-
-                        let countdownValue = 5;
-                        const countdownElement = document.getElementById("countdown");
-                        const countdownInterval = setInterval(function () {
-                            countdownValue -= 1;
-                            countdownElement.textContent = countdownValue;
-
-                            if (countdownValue === 0) {
-                                browser.management.uninstallSelf();
-                            }
-                        }, 1000);
-
-                        document.getElementById("cancelButton").addEventListener("click", function () {
-                            popup.style.display = "none";
-                            clearInterval(countdownInterval);
-                        });
+                        browser.management.uninstallSelf();
                     });
                 }
             }
@@ -55,5 +38,5 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
         // Not Firefox, load options.html directly
         window.location.href = 'options.html';
-    }
+    }    
 });
