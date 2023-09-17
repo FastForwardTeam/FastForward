bfacfd33a7bfcfed1bd80fd6fc8153802c42cab2
Ame <76390419+driedpampas@users.noreply.github.com>
2023-09-17 12:58:41 +0300

    update files

--- src/html/consent.js
+++ src/html/consent.js
@@ -5,51 +5,55 @@ async function saveConsentStatus(consentStatus) {
   
 // Function to get consent status
 async function getConsentStatus() {
-    return new Promise((resolve) => {
-      browser.storage.local.get('consentStatus').then((result) => {
-        resolve(result.consentStatus);
-      });
+return new Promise((resolve) => {
+    browser.storage.local.get('consentStatus').then((result) => {
+    resolve(result.consentStatus);
     });
+});
 }
-  
+
 const isFirefox = /Firefox/i.test(navigator.userAgent);
-  
-if (isFirefox) {
-    browser.runtime.onInstalled.addListener(async (details) => {
-        if (details.reason === "install") {
-            const consentStatus = await getConsentStatus();
 
-            if (consentStatus !== 'granted') {
-                document.getElementById("consentButton").addEventListener("click", async function () {
-                    await saveConsentStatus('granted');
-                    window.location.href = 'options.html';
-                });
+document.addEventListener("DOMContentLoaded", function () {
+    if (isFirefox) {
+        browser.runtime.onInstalled.addListener(async (details) => {
+            if (details.reason === "install") {
+                const consentStatus = await getConsentStatus();
 
-                document.getElementById("declineButton").addEventListener("click", async function () {
-                    await saveConsentStatus('declined');
-                    const popup = document.getElementById("popup");
-                    popup.style.display = "block";
-
-                    let countdownValue = 5;
-                    const countdownElement = document.getElementById("countdown");
-                    const countdownInterval = setInterval(function () {
-                        countdownValue -= 1;
-                        countdownElement.textContent = countdownValue;
-
-                        if (countdownValue === 0) {
-                            browser.management.uninstallSelf();
-                        }
-                    }, 1000);
-
-                    document.getElementById("cancelButton").addEventListener("click", function () {
-                        popup.style.display = "none";
-                        clearInterval(countdownInterval);
+                if (consentStatus !== 'granted') {
+                    const consentButton = document.getElementById("consentButton");
+                    consentButton.addEventListener("click", async function () {
+                        await saveConsentStatus('granted');
+                        window.location.href = 'options.html';
                     });
-                });
+
+                    const declineButton = document.getElementById("declineButton");
+                    declineButton.addEventListener("click", async function () {
+                        await saveConsentStatus('declined');
+                        const popup = document.getElementById("popup");
+                        popup.style.display = "block";
+
+                        let countdownValue = 5;
+                        const countdownElement = document.getElementById("countdown");
+                        const countdownInterval = setInterval(function () {
+                            countdownValue -= 1;
+                            countdownElement.textContent = countdownValue;
+
+                            if (countdownValue === 0) {
+                                browser.management.uninstallSelf();
+                            }
+                        }, 1000);
+
+                        document.getElementById("cancelButton").addEventListener("click", function () {
+                            popup.style.display = "none";
+                            clearInterval(countdownInterval);
+                        });
+                    });
+                }
             }
-        }
-    });
-} else {
-    // Not Firefox, load options.html directly
-    window.location.href = 'options.html';
-}
+        });
+    } else {
+        // Not Firefox, load options.html directly
+        window.location.href = 'options.html';
+    }
+});
