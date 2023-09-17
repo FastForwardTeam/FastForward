e72172a34bfe731b7bb0e878886a4b16d0ddbbd5
Ame <76390419+driedpampas@users.noreply.github.com>
2023-09-17 12:35:52 +0300

    update files

--- src/html/consent.js
+++ src/html/consent.js
@@ -1,42 +1,55 @@
-const isFirefox = /Firefox/i.test(navigator.userAgent);
-
-if (isFirefox) {
-    browser.storage.local.get('consentStatus').then(function (data) {
-        const consentStatus = data.consentStatus;
-
-        if (consentStatus !== 'granted') {
-            document.getElementById("consentButton").addEventListener("click", function () {
-                browser.storage.local.set({ 'consentStatus': 'granted' }).then(function () {
-                    window.location.href = 'options.html';
-                });
-            });
-
-            document.getElementById("declineButton").addEventListener("click", function () {
-                browser.storage.local.set({ 'consentStatus': 'declined' }).then(function () {
-                    const popup = document.getElementById("popup");
-                    popup.style.display = "block";
-
-                    let countdownValue = 5;
-                    const countdownElement = document.getElementById("countdown");
-                    const countdownInterval = setInterval(function () {
-                        countdownValue -= 1;
-                        countdownElement.textContent = countdownValue;
-
-                        if (countdownValue === 0) {
-                            browser.management.uninstallSelf();
-                        }
-                    }, 1000);
-
-                    document.getElementById("cancelButton").addEventListener("click", function () {
-                        popup.style.display = "none";
-                        clearInterval(countdownInterval);
-                    });
-                });
-            });
-        }
+// Function to save consent status
+async function saveConsentStatus(consentStatus) {
+    return browser.storage.local.set({ consentStatus: consentStatus });
+  }
+  
+  // Function to get consent status
+  async function getConsentStatus() {
+    return new Promise((resolve) => {
+      browser.storage.local.get('consentStatus').then((result) => {
+        resolve(result.consentStatus);
+      });
     });
-} else {
-    // Not Firefox, load options.html directly
-    window.location.href = 'options.html';
-}
-
+  }
+  
+  const isFirefox = /Firefox/i.test(navigator.userAgent);
+  
+  if (isFirefox) {
+      browser.runtime.onInstalled.addListener(async (details) => {
+          if (details.reason === "install") {
+              const consentStatus = await getConsentStatus();
+  
+              if (consentStatus !== 'granted') {
+                  document.getElementById("consentButton").addEventListener("click", async function () {
+                      await saveConsentStatus('granted');
+                      window.location.href = 'options.html';
+                  });
+  
+                  document.getElementById("declineButton").addEventListener("click", async function () {
+                      await saveConsentStatus('declined');
+                      const popup = document.getElementById("popup");
+                      popup.style.display = "block";
+  
+                      let countdownValue = 5;
+                      const countdownElement = document.getElementById("countdown");
+                      const countdownInterval = setInterval(function () {
+                          countdownValue -= 1;
+                          countdownElement.textContent = countdownValue;
+  
+                          if (countdownValue === 0) {
+                              browser.management.uninstallSelf();
+                          }
+                      }, 1000);
+  
+                      document.getElementById("cancelButton").addEventListener("click", function () {
+                          popup.style.display = "none";
+                          clearInterval(countdownInterval);
+                      });
+                  });
+              }
+          }
+      });
+  } else {
+      // Not Firefox, load options.html directly
+      window.location.href = 'options.html';
+  }
