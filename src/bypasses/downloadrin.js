67648dd3043de6ea51c861796b54710a1020b86e
supernova <76390419+driedpampas@users.noreply.github.com>
2023-09-20 18:30:01 +0300

    port: downloadr.in bypass

---
+++ src/bypasses/downloadrin.js
@@ -0,0 +1,16 @@
+import BypassDefinition from './BypassDefinition.js';
+
+export default class DownloadrIn extends BypassDefinition {
+    constructor() {
+        super();
+        // Custom bypass required bases can be set here
+    }
+
+    execute() {
+        // Extract query parameters from the current URL and safely navigate
+        const queryParams = new URL(location.href).search.slice(1);
+        safelyNavigate(queryParams);
+    }
+}
+
+export const matches = ['downloadr.in'];
