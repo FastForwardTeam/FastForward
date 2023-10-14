3d4f8da3d0fa0f3e3fe817dcb828ec28cac4ec40
T5ive <t5_@outlook.co.th>
2023-10-15 02:16:38 +0700

    Add tii.la

---
+++ src/bypasses/tii.js
@@ -0,0 +1,14 @@
+import BypassDefinition from './BypassDefinition.js'
+
+export default class Tii extends BypassDefinition {
+    constructor() {
+        super()
+        // custom bypass required bases can be set here
+    }
+
+    execute() {
+        // todo: make execute function
+    }
+}
+
+export const matches = ['tii.la']
