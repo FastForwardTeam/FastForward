7eef27ef3f50acf105eabac561838abdb45c8bae
nfal001 <abdurahman.nfal@gmail.com>
2023-10-24 15:46:07 +0700

    fix: miuiku, sekilastekno, add bypass vebma - miuiku.com - sekilastekno.com - vebma.com

--- src/bypasses/adtival.js
+++ src/bypasses/adtival.js
@@ -9,6 +9,82 @@ export default class Adtival extends BypassDefinition {
   execute() {
     console.log('Adtival found!');
 
+    const executor = async () => {
+      const El = window.livewire.components.components()[0];
+
+      const payload = {
+        fingerprint: El.fingerprint,
+        serverMemo: El.serverMemo,
+        updates: [
+          {
+            payload: {
+              event: 'getData',
+              id: 'whathappen',
+              params: [],
+            },
+            type: 'fireEvent',
+          },
+        ],
+      };
+
+      const response = await fetch(
+        location.origin + '/livewire/message/pages.show',
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Livewire': 'true',
+            'X-CSRF-TOKEN': window.livewire_token,
+          },
+          method: 'POST',
+          body: JSON.stringify(payload),
+        }
+      );
+
+      const json = await response.json();
+
+      // ensure URL
+      const url = new URL(json.effects.emits[0].params[0]);
+
+      this.helpers.safelyAssign(url.href);
+      // this.helpers.unsafelyNavigate(url.href, location.href)
+    };
+
+    // special case for sekilastekno. modbaca(?)
+    if (RegExp(/(modebaca|sekilastekno)\.com/).exec(location.host)) {
+      this.helpers.ifElement("form[method='post']", (a) => {
+        console.log('addRecord...');
+
+        const input = document.createElement('input');
+        input.value = window.livewire_token;
+        input.name = '_token';
+        input.hidden = true;
+        a.appendChild(input);
+        a.submit();
+      });
+
+      // ...same step as miuiku and vebma
+      this.helpers.ifElement('button[x-text]', async () => {
+        console.log('getLink..');
+        executor();
+      });
+
+      return;
+    }
+
+    // adtival getLink on miuiku
+    this.helpers.ifElement("div[class='max-w-5xl mx-auto']", async () => {
+      console.log('Executing..');
+      executor();
+    });
+
+    // adtival b64UrlLastPage
+    this.helpers.ifElement("button[id='copyVideoURL']", () => {
+      const shortID = new URLSearchParams(window.location.search).get(
+        'shortid'
+      );
+
+      this.helpers.safelyAssign(atob(shortID));
+    });
   }
 }
 
