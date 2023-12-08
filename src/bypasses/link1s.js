ecdfba66c72055c27a3ae803c030a473c6458784
Bayu Satiyo <itsyuukunz@gmail.com>
2023-12-08 11:18:17 +0700

    Better bypass for link1s.com
    - Skip all timer
    - Ad block bypass
    
    Signed-off-by: Bayu Satiyo <itsyuukunz@gmail.com>

--- src/bypasses/link1s.js
+++ src/bypasses/link1s.js
@@ -5,15 +5,30 @@ export default class Link1s extends BypassDefinition {
     super()
   }
   execute () {
-    this.helpers.watchForElement('#link1s[href]', a =>
-      this.helpers.safelyNavigate(a.href)
-    )
-    this.helpers.watchForElement('#link1s-snp button', b => b.click())
-    this.helpers.watchForElement('.skip-ad a.btn', a => {
-      setInterval(() => {
-        this.helpers.safelyNavigate(a.href)
-      }, 50) // wait for a moment for url to be updated
-    })
+    switch (window.location.hostname) {
+      case 'anhdep24.com':
+        this.helpers.watchForElement('.footer', () =>
+          clearInterval(window.counter) // skip timer
+        )
+        this.helpers.watchForElement('#link1s[href]', a =>
+          this.helpers.safelyNavigate(a.href) // step 1 & 3
+        )
+        this.helpers.watchForElement('#link1s-snp', () => window.link1sgo()) // step 2 & 4
+        break
+      case 'link1s.com':
+        this.helpers.ensureDomLoaded(() => {
+          // skip all shit
+          window.app_vars.counter_value = 0
+          window.app_vars.enable_captcha = 'no'
+          window.app_vars.force_disable_adblock = '0'
+        })
+        this.helpers.watchForElement('.skip-ad a.btn', a => {
+          setInterval(() => {
+            this.helpers.safelyNavigate(a.href)
+          }, 10) // wait for a moment for url to be updated
+        })
+        break
+    }
   }
 }
 
